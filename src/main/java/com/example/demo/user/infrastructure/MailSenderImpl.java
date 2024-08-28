891e1229f9c1efa959edcdb87223d3a682b0c1e8
coen <bht9011@gmail.com>
2024-08-28 21:26:49 +0900

    Dependency Inversion to mail sender

---
+++ src/main/java/com/example/demo/user/infrastructure/MailSenderImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo.user.infrastructure;
+
+import com.example.demo.user.service.port.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender {
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void send(String email, String title, String content) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject(title);
+        simpleMailMessage.setText(content);
+        javaMailSender.send(simpleMailMessage);
+    }
+}
