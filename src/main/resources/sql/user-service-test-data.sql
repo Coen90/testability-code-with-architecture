a927ff9e07de007e5201f318c97d67f5c1292459
coen <baghyeontae@baghyeontaeui-MacBookPro.local>
2024-08-28 16:16:33 +0900

    userservice test

---
+++ src/main/resources/sql/user-service-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'bht9011@gmail.com', 'coen', '서울시 광진구', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('2', 'bht9012@gmail.com', 'coen', '서울시 광진구', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'PENDING', 0);
--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,7 +1,74 @@
 package com.example.demo.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@SqlGroup({
+    @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+@Sql("/sql/user-service-test-data.sql")
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void getByEmail_은_ACTIVE_유저데이터를_찾아올_수_있다() {
+        //given
+        String email = "bht9011@gmail.com";
+
+        //when
+        UserEntity result = userService.getByEmail(email);
+
+        //then
+        assertThat(result.getNickname()).isEqualTo("coen");
+    }
+
+    @Test
+    void getByEmail_은_PENDING_유저데이터를_찾아올_수_없다() {
+        //given
+        String email = "bht9012@gmail.com";
+
+        //when
+        //then
+        assertThatThrownBy(() -> userService.getByEmail(email))
+            .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+
+    @Test
+    void getById_은_ACTIVE_유저데이터를_찾아올_수_있다() {
+        //given
+        String email = "bht9012@gmail.com";
+
+        //when
+        UserEntity result = userService.getById(1L);
+
+        //then
+        assertThat(result.getNickname()).isEqualTo("coen");
+    }
+
+    @Test
+    void getById_은_PENDING_유저데이터를_찾아올_수_없다() {
+        //given
+        String email = "bht9012@gmail.com";
+
+        //when
+        //then
+        assertThatThrownBy(() -> userService.getById(2L))
+            .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+
 
 }
