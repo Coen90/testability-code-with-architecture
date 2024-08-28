a8a53ccf2f19a8d9a5fce68ba1ccf863647579d2
coen <baghyeontae@baghyeontaeui-MacBookPro.local>
2024-08-28 15:53:37 +0900

    remove useless code

---
+++ src/main/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'bht9011@gmail.com', 'coen', '서울시 광진구', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저데이터를_찾아올_수_있다() {
+        Optional<UserEntity> byIdAndStatus = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
+
+        assertThat(byIdAndStatus.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        Optional<UserEntity> byIdAndStatus = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+        assertThat(byIdAndStatus.isPresent()).isFalse();
+        assertThat(byIdAndStatus.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저데이터를_찾아올_수_있다() {
+        Optional<UserEntity> byIdAndStatus = userRepository.findByEmailAndStatus("bht9011@gmail.com", UserStatus.ACTIVE);
+
+        assertThat(byIdAndStatus.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        Optional<UserEntity> byIdAndStatus = userRepository.findByEmailAndStatus("bht9011@gmail.com", UserStatus.PENDING);
+        assertThat(byIdAndStatus.isPresent()).isFalse();
+        assertThat(byIdAndStatus.isEmpty()).isTrue();
+    }
+
+}
